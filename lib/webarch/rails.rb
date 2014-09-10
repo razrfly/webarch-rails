c66142309bcf7469654bf4735c5f074ffbd91383
Szymon Madeja <smadeja1@gmail.com>
2014-09-10 12:05:22 +0200

    Initial commit

---
+++ lib/webarch/rails.rb
@@ -0,0 +1,8 @@
+require "webarch/rails/version"
+
+module Webarch
+  module Rails
+    class Engine < ::Rails::Engine
+    end
+  end
+end
---
+++ vendor/assets/javascripts/webarch-rails.js
@@ -0,0 +1,26 @@
+//
+//= require twitter/bootstrap
+//
+//= require ../webarch-deps/breakpoints/breakpoints
+//= require ../webarch-deps/jquery-unveil/jquery.unveil.min
+//
+//= require ../webarch-deps/jquery.cookie/jquery.cookie.js
+//
+//= require ../webarch-deps/jquery-block-ui/jqueryblockui
+//= require ../webarch-deps/jquery-slider/jquery.sidr.min
+//= require ../webarch-deps/jquery-slimscroll/jquery.slimscroll.min
+//= require ../webarch-deps/pace/pace.min
+//= require ../webarch-deps/jquery-numberAnimate/jquery.animateNumbers
+//
+//  require ../webarch-deps/bootstrap-select2/select2.min
+//= require ../webarch-deps/select2-3.4.8/select2.min
+//
+//= require ../webarch-deps/jquery-datatable/js/jquery.dataTables.min
+//= require ../webarch-deps/jquery-datatable/extra/js/dataTables.tableTools.min
+//= require ../webarch-deps/datatables-responsive/js/datatables.responsive.js
+//= require ../webarch-deps/datatables-responsive/js/lodash.min.js
+//
+//= require webarch/datatables
+//= require webarch/core
+//= require webarch/chat
+//
